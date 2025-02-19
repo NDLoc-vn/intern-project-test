232607d0334e88b4d753bf964f7b02505e847369
NDLoc-vn <loccoc1807@gmail.com>
2025-02-19 08:50:26 +0700

    setting up server with dotnet and connect to MariaDb using Code-First approach

---
+++ server/Models/ApplicationContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
+{
+    public DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>().HasData(
+            new Student
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Address = "123 Main St",
+            },
+            new Student
+            {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Doe",
+                Address = "456 Oak St",
+            }
+        );
+    }
+}
